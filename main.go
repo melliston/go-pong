1ea4c4abb5daa54159d558c1c1d485741dc4914c
Matthew Elliston <mpgelliston@gmail.com>
2022-09-02 16:59:47 +0100

    Added the new intro screen

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"math/rand"
 	"os"
 	"strconv"
 	"strings"
@@ -10,12 +11,18 @@ import (
 	"github.com/gdamore/tcell/v2"
 )
 
+var INTRO_MESSAGE = "GoPong - By Matthew Elliston"
+var INTRO_START = "Press 'Spacebar' To Start"
+var RESET_MESSAGE = "Press Space To Reset Game"
+
 type Game struct {
 	Screen      tcell.Screen
 	Ball        Ball
 	P1          Player
 	P2          Player
 	TargetScore int
+	DemoMode    bool
+	GameStarted bool
 }
 
 type Player struct {
@@ -72,14 +79,21 @@ func main() {
 			if event.Key() == tcell.KeyEscape || event.Key() == tcell.KeyCtrlC {
 				screen.Fini()
 				os.Exit(0)
-			} else if event.Key() == tcell.KeyUp {
-				game.P2.Paddle.MoveUp(0)
-			} else if event.Key() == tcell.KeyDown {
-				game.P2.Paddle.MoveDown(height)
-			} else if event.Rune() == 'w' {
-				game.P1.Paddle.MoveUp(0)
-			} else if event.Rune() == 's' {
-				game.P1.Paddle.MoveDown(height)
+			} else if event.Key() == tcell.KeyUp && game.GameStarted {
+				game.P2.Paddle.MoveUp(game.P2.Paddle.YSpeed, 0)
+			} else if event.Key() == tcell.KeyDown && game.GameStarted {
+				game.P2.Paddle.MoveDown(game.P2.Paddle.YSpeed, height)
+			} else if event.Rune() == 'w' && game.GameStarted {
+				game.P1.Paddle.MoveUp(game.P1.Paddle.YSpeed, 0)
+			} else if event.Rune() == 's' && game.GameStarted {
+				game.P1.Paddle.MoveDown(game.P1.Paddle.YSpeed, height)
+			} else if event.Rune() == ' ' && game.CheckGameOver() {
+				game.Init()
+				go game.Loop()
+			} else if event.Rune() == ' ' && !game.GameStarted {
+				game.GameStarted = true
+				game.DemoMode = false
+				game.Init()
 			}
 		}
 	}
@@ -130,6 +144,7 @@ func (g *Game) Init() {
 	g.P1 = p1
 	g.P2 = p2
 	g.TargetScore = 9
+	g.DemoMode = true
 }
 
 func (g *Game) Loop() {
@@ -145,10 +160,15 @@ func (g *Game) Loop() {
 
 		// Check game over
 		if g.CheckGameOver() {
-			drawSprite(g.Screen, (width/2)-4, (height / 2), (width/2)+5, (height / 2), defStyle, "Game Over")
-			drawSprite(g.Screen, (width/2)-(len(g.WinnerString())/2), (height/2)+2, (width/2)+(len(g.WinnerString())+1/2), (height/2)+2, defStyle, g.WinnerString())
-			g.Screen.Show()
-			return
+			if g.GameStarted {
+				drawSprite(g.Screen, (width/2)-4, (height / 2), (width/2)+5, (height / 2), defStyle, "Game Over")
+				drawSprite(g.Screen, (width/2)-(len(g.WinnerString())/2), (height/2)+2, (width/2)+(len(g.WinnerString())+1/2), (height/2)+2, defStyle, g.WinnerString())
+				drawSprite(g.Screen, (width/2)-(len(RESET_MESSAGE)/2), (height/2)+8, (width/2)+(len(RESET_MESSAGE)+1/2), (height/2)+8, defStyle, RESET_MESSAGE)
+				g.Screen.Show()
+				return
+			} else {
+				g.Init()
+			}
 		}
 
 		// Update the ball position
@@ -158,6 +178,35 @@ func (g *Game) Loop() {
 		drawSprite(g.Screen, g.Ball.Sprite.X, g.Ball.Sprite.Y, g.Ball.Sprite.X+g.Ball.Sprite.Width, g.Ball.Sprite.Y+g.Ball.Sprite.Height, defStyle, g.Ball.Draw())
 		drawSprite(g.Screen, g.P1.Paddle.Sprite.X, g.P1.Paddle.Sprite.Y, g.P1.Paddle.Sprite.X+g.P1.Paddle.Sprite.Width, g.P1.Paddle.Sprite.Y+g.P1.Paddle.Sprite.Height, paddleStyle, g.P1.Paddle.Draw())
 		drawSprite(g.Screen, g.P2.Paddle.Sprite.X, g.P2.Paddle.Sprite.Y, g.P2.Paddle.Sprite.X+g.P2.Paddle.Sprite.Width, g.P2.Paddle.Sprite.Y+g.P2.Paddle.Sprite.Height, paddleStyle, g.P2.Paddle.Draw())
+
+		// If DemoMode lets draw the Intro Screen
+		if g.DemoMode && !g.GameStarted {
+			drawSprite(g.Screen, (width/2)-(len(INTRO_MESSAGE)/2), (height / 2), (width/2)+(len(INTRO_MESSAGE)+1/2), (height / 2), defStyle, INTRO_MESSAGE)
+			drawSprite(g.Screen, (width/2)-(len(INTRO_START)/2), (height/2)+4, (width/2)+(len(INTRO_START)+1/2), (height/2)+4, defStyle, INTRO_START)
+			if g.Ball.XSpeed > 0 {
+				// Update Paddle 2
+				if g.Ball.YSpeed > 0 {
+					if g.Ball.Sprite.Y > g.P2.Paddle.Sprite.Y {
+						g.P2.Paddle.MoveDown(rand.Intn(g.P2.Paddle.YSpeed), height)
+					}
+				} else {
+					if g.Ball.Sprite.Y < g.P2.Paddle.Sprite.Y {
+						g.P2.Paddle.MoveUp(rand.Intn(g.P2.Paddle.YSpeed), 0)
+					}
+				}
+			} else {
+				// Update Paddle 1
+				if g.Ball.YSpeed > 0 {
+					if g.Ball.Sprite.Y > g.P1.Paddle.Sprite.Y {
+						g.P1.Paddle.MoveDown(rand.Intn(g.P1.Paddle.YSpeed), height)
+					}
+				} else {
+					if g.Ball.Sprite.Y < g.P1.Paddle.Sprite.Y {
+						g.P1.Paddle.MoveUp(rand.Intn(g.P1.Paddle.YSpeed), 0)
+					}
+				}
+			}
+		}
 
 		// Scores
 		drawSprite(g.Screen, 10, 1, 1, 1, defStyle, strconv.Itoa(g.P1.Score))
@@ -229,7 +278,7 @@ func drawSprite(screen tcell.Screen, x1, y1, x2, y2 int, style tcell.Style, spri
 // CheckCollision Checks to see if there is a collision between two sprites
 func checkCollision(sprite1, sprite2 Sprite) bool {
 	if sprite1.X >= sprite2.X &&
-		sprite1.X <= sprite2.X+sprite2.Width &&
+		sprite1.X <= (sprite2.X+sprite2.Width)-1 && // Deducted 1 as it seemed to pass through the element then get the collision.
 		sprite1.Y >= sprite2.Y &&
 		sprite1.Y <= sprite2.Y+sprite2.Height {
 		return true
@@ -248,6 +297,7 @@ func (b *Ball) Update() {
 }
 
 func (b *Ball) Reset(x, y, xSpeed, ySpeed int) {
+	// TODO Add Randomisation to the starting Y position of the ball.
 	b.Sprite.X = x
 	b.Sprite.Y = y
 	b.XSpeed = xSpeed
@@ -274,15 +324,15 @@ func (p *Paddle) Draw() string {
 
 // MoveUp Moves the Paddle Up,
 // minHeight is passed so that it will allow for the game area to be different to the screen size.
-func (p *Paddle) MoveUp(minHeight int) {
+func (p *Paddle) MoveUp(speed, minHeight int) {
 	if p.Sprite.Y > minHeight {
-		p.Sprite.Y -= p.YSpeed
+		p.Sprite.Y -= speed
 	}
 }
 
 // MoveDown Moves the Paddle Down
-func (p *Paddle) MoveDown(maxHeight int) {
+func (p *Paddle) MoveDown(speed, maxHeight int) {
 	if p.Sprite.Y < maxHeight-p.Sprite.Height {
-		p.Sprite.Y += p.YSpeed
+		p.Sprite.Y += speed
 	}
 }
